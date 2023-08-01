ca8a9b6ff09936a9d17d4674b0aed7e1c2d599a3
Umer Abdullah Yaseen <umerabdillah6223@gmail.com>
2023-08-01 15:20:24 +0500

    Added Sorting and Filtering

---
+++ src/Pages/ProductPage.js
@@ -0,0 +1,12 @@
+import { CheckOut } from "../features/Checkout/CheckOut";
+import ProductList from "../features/ProductList/components/ProductList";
+
+const CheckOutPage = () => {
+  return (
+    <>
+      <ProductList />
+    </>
+  );
+};
+
+export default CheckOutPage;
